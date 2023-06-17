9e63ff350d39309bd4fb285384a567aff8054380
VoltageFeedback <suyaoxian@gmail.com>
2023-06-17 06:04:52 -0400

    Fixed b_canshi ai check

--- character/balance/b_sunhao.js
+++ character/balance/b_sunhao.js
@@ -1,26 +1,23 @@
 'use strict';
-import {game} from '/game/game.js';
-import {lib} from '/game/game.js';
-import {ui} from '/game/game.js';
-import {get} from '/game/game.js';
-import {ai} from '/game/game.js';
-import {_status} from '/game/game.js';
+import { game } from '/game/game.js';
+import { lib } from '/game/game.js';
+import { ui } from '/game/game.js';
+import { get } from '/game/game.js';
+import { ai } from '/game/game.js';
+import { _status } from '/game/game.js';
 
 
 const b_sunhao = {
 	character: {
-		b_sunhao: ['male', 'wu', 5, ['b_recanshi', 'rechouhai', 'b_guiming'], ['zhu']],
+		b_sunhao: ['male', 'wu', 5, ['b_canshi', 'rechouhai', 'b_guiming'], ['zhu']],
 
 	},
-	characterIntro: {
-		b_sunhao: '孙权之孙，孙和之子，东吴的末代君主。在位初期虽施行过明政，但不久即沉溺酒色，专于杀戮，变得昏庸暴虐，嗜用挖眼、剥皮等酷刑。280年，吴国被西晋所灭，孙皓投降西晋，被封为归命侯。',
-
-	},
+	characterIntro: {},
 	characterReplace: {
 		b_sunhao: ['b_sunhao', 'sunhao'],
 	},
 	skill: {
-		b_recanshi: {
+		b_canshi: {
 			audio: 'canshi',
 			trigger: { player: 'phaseDrawBegin2' },
 			check: function (event, player) {
@@ -31,7 +28,7 @@ const b_sunhao = {
 				if (player.skipList.contains('phaseUse') || !handcardTrickCount) return true;
 				// To check if player can avoid no handcards at end
 				const currentHandcardCount = player.countCards('h');
-				const drawCardCount = player.draw.num;
+				const drawCardCount = event.num;
 				let extraDrawCardCount = game.countPlayer(function (current) {
 					if (player.hasZhuSkill('b_guiming') && current.group == 'wu' && current != player) return true;
 					return current.isDamaged() && current != player;
@@ -44,12 +41,13 @@ const b_sunhao = {
 					}
 					return false;
 				});
-				if (currentHandcardCount == minGlobalHandcardCount+1) return true;
-				if ((currentHandcardCount+drawCardCount+extraDrawCardCount) - (2*handcardTrickCount-minGlobalHandcardCount-1) > 0) {
+				if (currentHandcardCount == minGlobalHandcardCount + 1) return true;
+				if ((currentHandcardCount + drawCardCount + extraDrawCardCount) - (2 * handcardTrickCount - minGlobalHandcardCount - 1) > 0) {
 					return true;
 				};
 				return false;
 			},
+			// Prompt title
 			prompt: function (event, player) {
 				var num = game.countPlayer(function (current) {
 					if (player.hasZhuSkill('b_guiming') && current.group == 'wu' && current != player) return true;
@@ -57,11 +55,12 @@ const b_sunhao = {
 				});
 				return '残蚀：是否多摸' + get.cnNumber(num + 1) + '张牌？';
 			},
+			// Prompt content
 			prompt2: function (event, player) {
 				if (player.hasZhuSkill('b_guiming')) {
 					return '摸牌阶段开始时，你可以多摸X+1张牌（X为其他已受伤角色或其他吴势力角色），若如此做，当你于此回合内使用【杀】或普通锦囊牌时，若你手牌数不为全场最少之一，你弃置一张牌。';
 				}
-				return lib.translate['b_recanshi_info'];
+				return lib.translate['b_canshi_info'];
 			},
 			content: function () {
 				var num = game.countPlayer(function (current) {
@@ -69,10 +68,10 @@ const b_sunhao = {
 					return current.isDamaged() && current != player;
 				});
 				trigger.num += num + 1;
-				player.addTempSkill('b_recanshi2');
+				player.addTempSkill('b_canshi2');
 			}
 		},
-		b_recanshi2: {
+		b_canshi2: {
 			trigger: { player: 'useCard' },
 			forced: true,
 			filter: function (event, player) {
@@ -114,9 +113,9 @@ const b_sunhao = {
 	translate: {
 		"balance/index": '平衡',
 		b_sunhao: '衡孙皓',
-		b_recanshi: '残蚀',
-		b_recanshi2: '残蚀',
-		b_recanshi_info: '摸牌阶段开始时，你可以多摸X+1张牌（X为其他已受伤角色数），若如此做，当你于此回合内使用【杀】或普通锦囊牌时，若你手牌数不为全场最少之一，你弃置一张牌。',
+		b_canshi: '残蚀',
+		b_canshi2: '残蚀',
+		b_canshi_info: '摸牌阶段开始时，你可以多摸X+1张牌（X为其他已受伤角色数），若如此做，当你于此回合内使用【杀】或普通锦囊牌时，若你手牌数不为全场最少之一，你弃置一张牌。',
 		// rechouhai: '仇海',
 		// rechouhai_info: '锁定技，当你受到渠道为【杀】的伤害时，若你没有手牌，此伤害+1。',
 		b_guiming: '归命',
@@ -125,7 +124,7 @@ const b_sunhao = {
 	},
 };
 
-export {b_sunhao};
+export { b_sunhao };
 
 
 
